8b49367bf382875dc0bf2d8e1bfaf5840303d447
Gigixdeborah <gigixdeborah@gmail.com>
2025-06-08 13:00:36 +0100

    Add random import

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+import random
 from datetime import datetime, timedelta
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
